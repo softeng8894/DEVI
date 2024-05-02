6f82737941abc9ee269007bc244e01648dfadb31
himanshu <softeng8894@gmail.com>
2024-05-02 14:27:54 +0530

    first commit ok done2

--- src/test/java/docker.java
+++ src/test/java/docker.java
@@ -13,18 +13,13 @@ public class docker {
 	@Test
 	public void hpd() throws MalformedURLException, URISyntaxException
 	{
-	    //ChromeOptions options = new ChromeOptions();
-	    //RemoteWebDriver driver= new RemoteWebDriver(new URI("http://localhost:4444/wd/hub").toURL(), options);
-        //System.out.println("Browser open");
-        //driver.get("https://www.google.com");
-        //System.out.println(driver.getTitle());
-	    //System.out.println("Browser close");
-		//System.out.println("Done che boss");
-	    //driver.quit();
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+	    ChromeOptions options = new ChromeOptions();
+	    RemoteWebDriver driver= new RemoteWebDriver(new URI("http://localhost:4444/wd/hub").toURL(), options);
+        System.out.println("Browser open");
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+	    System.out.println("Browser close");
 		System.out.println("Done che boss");
-		driver.quit();
+	    driver.quit();
 	}
 }
