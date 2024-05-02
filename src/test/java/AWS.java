34b0eb58bda102fa6979513e63fcd425eb0fb73f
himanshu <softeng8894@gmail.com>
2024-05-02 14:43:23 +0530

    first commit ok done8

--- src/test/java/AWS.java
+++ src/test/java/AWS.java
@@ -8,15 +8,21 @@ public class AWS {
 
     @Test
     public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
+        //WebDriver driver;
+        //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        //options.addArguments("disable-gpu");
+        //options.addArguments("--no-sandbox");
+        //options.addArguments("--disable-dev-shm-usage");
+        //driver = new ChromeDriver(options);
+        //driver.get("https://www.google.com");
+        //System.out.println("Title of the page is: " + driver.getTitle());
+    	
+    	   System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+           WebDriver driver = new ChromeDriver();
+           driver.get("https://www.example.com");
+           System.out.println("Page title is: " + driver.getTitle());
+           driver.quit();
     }
 }
