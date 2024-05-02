d64c2c48246f2c2f452324a59cdfc91818837e79
himanshu <softeng8894@gmail.com>
2024-05-02 16:22:10 +0530

    first commit ok done28

--- src/test/java/AWS.java
+++ src/test/java/AWS.java
@@ -22,6 +22,8 @@ public class AWS {
     	   System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
     	   ChromeOptions options = new ChromeOptions();
     	   options.addArguments("--headless");
+    	   options.addArguments("--no-sandbox");
+    	   options.addArguments("--disable-dev-shm-usage");
            WebDriver driver = new ChromeDriver(options);
            driver.get("https://www.example.com");
            System.out.println("Page title is: " + driver.getTitle());
