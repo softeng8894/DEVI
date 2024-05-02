6668908b5ea54ac3ac0c826a71bcf53ccc82f4ad
himanshu <softeng8894@gmail.com>
2024-05-02 14:23:30 +0530

    first commit ok done1

--- src/test/java/docker.java
+++ src/test/java/docker.java
@@ -2,6 +2,8 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -11,13 +13,18 @@ public class docker {
 	@Test
 	public void hpd() throws MalformedURLException, URISyntaxException
 	{
-	    ChromeOptions options = new ChromeOptions();
-	    RemoteWebDriver driver= new RemoteWebDriver(new URI("http://localhost:4444/wd/hub").toURL(), options);
-        System.out.println("Browser open");
-        driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
-	    System.out.println("Browser close");
+	    //ChromeOptions options = new ChromeOptions();
+	    //RemoteWebDriver driver= new RemoteWebDriver(new URI("http://localhost:4444/wd/hub").toURL(), options);
+        //System.out.println("Browser open");
+        //driver.get("https://www.google.com");
+        //System.out.println(driver.getTitle());
+	    //System.out.println("Browser close");
+		//System.out.println("Done che boss");
+	    //driver.quit();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 		System.out.println("Done che boss");
-	    driver.quit();
+		driver.quit();
 	}
 }
