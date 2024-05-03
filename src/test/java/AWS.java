2db4ab4308f7dd9fe5e05270072aaef6d313781e
himanshu <softeng8894@gmail.com>
2024-05-03 13:51:42 +0530

    ok12

--- src/test/java/AWS.java
+++ src/test/java/AWS.java
@@ -12,7 +12,6 @@ public class AWS {
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
