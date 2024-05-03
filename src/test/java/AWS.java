a8673586c925193374b7ea2180936b3c6dfa812b
himanshu <softeng8894@gmail.com>
2024-05-03 09:38:42 +0530

    ok2

--- src/test/java/AWS.java
+++ src/test/java/AWS.java
@@ -8,30 +8,15 @@ public class AWS {
 
     @Test
     public void OpenBrowser()  {
-        //WebDriver driver;
-        //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
-        //options.addArguments("disable-gpu");
-        //options.addArguments("--no-sandbox");
-        //options.addArguments("--disable-dev-shm-usage");
-        //driver = new ChromeDriver(options);
-        //driver.get("https://www.google.com");
-        //System.out.println("Title of the page is: " + driver.getTitle());
-    	
-    	   System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-    	   ChromeOptions options = new ChromeOptions();
-    	   options.addArguments("--headless");
-    	   options.addArguments("--no-sandbox");
-    	   options.addArguments("--disable-dev-shm-usage");
-    	   //options.addArguments("start-maximized"); 
-    	   //options.addArguments("disable-infobars"); 
-    	   //options.addArguments("--disable-extensions"); 
-    	   //options.addArguments("--disable-gpu"); 
-           WebDriver driver = new ChromeDriver(options);
-           driver.get("https://www.example.com");
-           System.out.println("Page title is: " + driver.getTitle());
-           driver.quit();
-
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
     }
 }
