4b2b84543c06fe748a6832a5616f4c330235a5d2
himanshu <softeng8894@gmail.com>
2024-05-06 15:24:29 +0530

    Yup8

--- src/test/java/AWS.java
+++ src/test/java/AWS.java
@@ -13,10 +13,11 @@ public class AWS {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        //options.addArguments("--no-sandbox");
-        //options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
+        System.out.println("Chrome browser is open");
         System.out.println("Title of the page is: " + driver.getTitle());
+        driver.quit();
+        System.out.println("Chrome browser is close");
     }
 }
